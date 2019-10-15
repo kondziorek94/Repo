e372be27479ffae0b71feda869a6c9722f65b7a2
Konrad Budny <kbudny492@gmail.com>
2019-10-14 19:05:03 -0500

    Yoda speech translator api

--- CarRentalWebApp/CarRentalWebApp/Scripts/apiSample.js
+++ CarRentalWebApp/CarRentalWebApp/Scripts/apiSample.js
@@ -1,19 +1,44 @@
 ﻿$(window).on("load", function () {
+    function trim(result) {
+        result = result.replace("{\"success\":{\"total\":1},\"contents\":{\"translated\":", "");
+        result = result.replace(",\"text\":\"", "");
+        result = result.replace($("#YodaInput").val(), "");
+        result = result.replace("\",\"translation\":\"yoda\"}}","");
+        return result;
+    }
     $("#apiTest").keypress(function (e) {
         if (e.which === 13) {
             $.ajax({
                 url: "http://numbersapi.com/" + $("#apiTest").val(),
-                
+
                 success: function (result, status, xhr) {
 
                     $("#apiSampleOutput").text(result);
                 },
                 error: function (result, status, xhr) {
-                    
+
                 }
             });
         }
 
+    });
+    $("#YodaInput").keypress(function (e) {
+        if (e.which === 13) {
+            $.ajax({
+                url: "https://api.funtranslations.com/translate/yoda.json?text=" + $("#YodaInput").val(),
+
+                success: function (result, status, xhr) {
+                    result = JSON.stringify(result);
+                    result = trim(result);
+                    //result=trim(result);
+                    $("#YodaOutput").text(result);
+                },
+                error: function (result, status, xhr) {
+
+                }
+            }
+            );
+        }
     });
 });
 $(window).trigger("onload");
